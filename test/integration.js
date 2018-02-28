1964e23924642812467b226960fb97fcbf8a14bf
Tobias <tobias@vergic.com>
2018-02-28 12:42:07 +0100

    Updated .gitignore Updated npm versions Fixed test for new doT version

--- test/integration.js
+++ test/integration.js
@@ -1,12 +1,12 @@
 var dot = require('../');
 var gutil = require('gulp-util');
 
-describe('gulp-dot-precompiler', function () {
+describe('gulp-dot-precompiler3', function () {
   it('should process files with default options just as expected :)', function (done) {
     var stream = dot();
 
     stream.on('data', function (file) {
-      file.contents.toString().should.equal('render["name"] = function anonymous(data) {\nvar out=\'\';return out;\n};');
+      file.contents.toString().should.equal('render["name"] = function anonymous(data\n/*``*/) {\nvar out=\'\';return out;\n};');
     });
     stream.once('end', function () {
       done();
